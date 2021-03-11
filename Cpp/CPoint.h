32bc8989f62685a0b4e4bd9b9a0579613031b71a
marine schwartz <m.schwartz@ludus-academie.com>
2021-03-11 12:26:35 +0100

    Ajoutez des fichiers projet.

---
+++ Cpp/CPoint.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class CPoint {
+
+private :
+	//donnees membres = attributs = pptes
+	int n_x;
+	int n_y;
+
+public :
+	//fonctions membres
+	void afficheLog() const;
+
+	//accesseurs
+	int getX()const;
+	int getY()const;
+
+	//mutateurs
+	void setX(int n_x);
+	void setY(int n_y);
+};
